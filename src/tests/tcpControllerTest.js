680a9472494c29cc4535f4ce0f92530b722fdaec
Vitali Matetski <vitalymatecky@gmail.com>
2015-12-17 01:41:11 +0300

    - Test for tcp controller was added.

---
+++ src/tests/tcpControllerTest.js
@@ -0,0 +1,5 @@
+var TrafficLight = require('./../trafficLight');
+var TcpController = require('./../tcpController');
+
+//var controller = new UdpController(new TrafficLight(17, 27, 22));
+//controller.setup();
